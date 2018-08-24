a07f02a86d6a64ed3e03abeec89852ef9f3fdcc2
Elliott Marquez <emarquez@google.com>
2018-08-24 10:50:59 -0700

    set up super basics

---
+++ src/secret-hitler.ts
@@ -0,0 +1,11 @@
+import { LitElement, html } from "@polymer/lit-element";
+
+class SecretHitler extends LitElement {
+  render() {
+    return html`
+      <div>I am secretly hitler!</div>
+    `;
+  }
+}
+
+customElements.define('secret-hitler', SecretHitler);
